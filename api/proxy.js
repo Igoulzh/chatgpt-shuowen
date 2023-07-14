77ca91f83ca7eb17298822b453cb00c1989762ab
lizhiheng <lizhiheng>
2023-07-14 15:45:15 +0800

    修改

--- api/proxy.js
+++ api/proxy.js
@@ -3,7 +3,7 @@ import { createProxyMiddleware } from 'http-proxy-middleware'
 export default (req, res) => {
   let target = ''
   if (req.url.startsWith('/api'))
-    target = process.env.VITE_APP_API_BASE_URL
+    target = 'https://47.96.179.163:3002'
 
   createProxyMiddleware({
     target,
